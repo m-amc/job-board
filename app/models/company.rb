39c56b7d914773bb8f86837a6e209fb42f85a185
Ana <anamorales.dev@gmail.com>
2020-11-19 19:37:18 -0500

    Add validation to Company model and added unit tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
+# Company model definition
 class Company < ApplicationRecord
+  validates :name,
+            uniqueness: { case_sensitive: false },
+            length: {
+              minimum: 8,
+              maximum: 30
+            }
+  validates :name, :address, :city, :state,
+            :country_code, :contact_number, presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
---
+++ test/models/company_test.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CompanyTest < ActiveSupport::TestCase
+  def setup
+    @company = Company.new(
+      name: 'ABC Company',
+      address: 'Main st',
+      city: 'Toronto',
+      state: 'ON',
+      country_code: 'CA',
+      contact_number: '416-000-0000',
+      email: 'abc@example.com'
+    )
+  end
+
+  test 'company name should be valid' do
+    assert @company.valid?
+  end
+
+  test 'name should be present' do
+    @company.name = " "
+    assert_not @company.valid?
+  end
+
+  test 'address should be present' do
+    @company.address = " "
+    assert_not @company.valid?
+  end
+
+  test 'city should be present' do
+    @company.city = ' '
+    assert_not @company.valid?
+  end
+
+  test 'state should be present' do
+    @company.state = ' '
+    assert_not @company.valid?
+  end
+
+  test 'country should be present' do
+    @company.country_code = ' '
+    assert_not @company.valid?
+  end
+
+  test 'contact number should be present' do
+    @company.contact_number = ' '
+    assert_not @company.valid?
+  end
+
+  test 'name should be unique' do
+    # Save first
+    @company.save
+
+    # Create a duplicate name
+    @company_duplicate =  @company = Company.new(
+      name: 'ABC COMPANY',
+      address: 'Main st',
+      city: 'Toronto',
+      state: 'ON',
+      country_code: 'CA',
+      contact_number: '416-000-0000',
+      email: 'abc@example.com'
+    )
+
+    assert_not @company.valid?
+  end
+
+  test 'name should not be too short (min 8 characters)' do
+    @company.name = 'a' * 3
+    assert_not @company.valid?
+  end
+
+  test 'name should not be too long (max 30 characters)' do
+    @company.name = 'a' * 35
+    assert_not @company.valid?
+  end
+
+  test 'email address format is correct' do
+    @company.email = 'abc.example.com'
+    assert_not @company.valid?
+  end
+end
