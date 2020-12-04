f194ac0c0d1894630a12172c6428b313f8dee916
Ana <anamorales.dev@gmail.com>
2020-12-03 21:33:20 -0500

    Configure rails for react (frontend)

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# This is the controller when the user goes to the root path
+class PagesController < ApplicationController
+  # index action
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Root path of the app
+  # When the user goes to the root path, rails will look for hte pages controller and index action
+  root 'pages#index'
 end
