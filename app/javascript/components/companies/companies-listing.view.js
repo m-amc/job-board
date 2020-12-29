337bb88e41b01317e4455e297185641de9a388e6
Ana <anamorales.dev@gmail.com>
2020-12-29 11:02:29 -0500

    Start work on CompaniesListing component dispatching action and rendering data from store using react-redux useDispatch and useSelector

--- app/javascript/components/companies/companies-listing.view.js
+++ app/javascript/components/companies/companies-listing.view.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCompanies } from '../../actions'
 
-export const CompaniesListing = () => (
-  <div>This is the Companies#index view</div>
-)
+export const CompaniesListing = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCompanies())
+  }, [])
+
+  // Get the companies data object from the companies reducer state (state.companies) and return the companies object (state.companies.companies).  We want to get the actual array so state.companies.companies.data
+  const companies = useSelector(state => state.companies.companies);
+
+  return (
+    <>
+      <div>This is the Companies#index view</div>
+      <ul>
+        {companies.map(company => {
+          const companyAttributes = company.attributes
+          return <li key={company.id}>{companyAttributes.name}</li>
+        })}
+      </ul>
+    </>
+  )
+}
+
+// connect will "connect" our component to the Provider so we can get data from the store. connect will pass the data to our component to render.
+// export default connect()(CompaniesListing);
+
